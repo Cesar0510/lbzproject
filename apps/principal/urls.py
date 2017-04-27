635c6bb3a67d060288e3dfa192c49e7ed3e4b564
Cesar Augusto <bash5426@gmail.com>
2017-04-26 19:43:17 -0500

    add home class view

--- apps/principal/urls.py
+++ apps/principal/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
+from django.views.generic import TemplateView
 
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'^$', TemplateView.as_view(template_name='principal/home.html'),
+         name='index'), 
+    #url(r'^$', views.index, name='index'), # func View
 ]
--- lbzproject/settings/base.py
+++ lbzproject/settings/base.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+ROOT_DIR = os.path.dirname(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'lbzproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(ROOT_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
